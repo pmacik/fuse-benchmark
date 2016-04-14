52841c518c5b1d0a5c2f172c9400c83dd2e5dd9e
Pavel Macík <pavel.macik@gmail.com>
2016-04-14 13:07:46 +0200

    Removed debugging output from MessageBean in camel-mqtt

--- camel-mqtt/src/main/java/org/pmacik/fuse/benchmark/MessageBean.java
+++ camel-mqtt/src/main/java/org/pmacik/fuse/benchmark/MessageBean.java
@@ -2,7 +2,6 @@ package org.pmacik.fuse.benchmark;
 
 public class MessageBean {
    public String sayHello(String msg) {
-      System.out.println("Hello" + msg);
       return "Hello " + msg;
    }
 }
